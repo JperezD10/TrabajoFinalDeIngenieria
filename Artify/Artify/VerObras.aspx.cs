08a7411d69b82dc95b3641e35f7167db86aaaf5a
JPerezD10 <jperezdemonty@live.com.ar>
2025-11-02 20:08:14 -0300

    XML Obra por artista

--- Artify/Artify/VerObras.aspx.cs
+++ Artify/Artify/VerObras.aspx.cs
@@ -4,10 +4,13 @@ using BLL;
 using SEGURIDAD;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml.Linq;
 
 namespace Artify
 {
@@ -23,15 +26,50 @@ namespace Artify
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack)
-            {
-                Context.Items["priceLabel"] = IdiomaManager.Instance.T("verobras.litPriceLabel");
+            if (IsPostBack) return;
 
-                var data = obraBLL.ListarObrasActivas();
-                BindObras(data);
+            Context.Items["priceLabel"] = IdiomaManager.Instance.T("verobras.litPriceLabel");
+
+            var data = obraBLL.ListarObrasActivas();
+            BindObras(data);
+
+            ExportarObrasPorArtistaXml(data);
+        }
+        private void ExportarObrasPorArtistaXml(IEnumerable<Obra> obras)
+        {
+            try
+            {
+                if (obras == null || !obras.Any())
+                    return;
+
+                var xml = new XDocument(
+                    new XElement("Obras",
+                        obras.Select(o =>
+                            new XElement("Obra",
+                                new XAttribute("Id", o.Id),
+                                new XAttribute("Titulo", o.Titulo),
+                                new XAttribute("Anio", o.Anio),
+                                new XAttribute("ArtistaId", o.ArtistaId),
+                                new XAttribute("Activo", o.Activo)
+                            )
+                        )
+                    )
+                );
+
+                string folderPath = @"C:\SqlBackups\Artify";
+                if (!Directory.Exists(folderPath))
+                    Directory.CreateDirectory(folderPath);
+
+                string fileName = $"ObrasPorArtista_{DateTime.Now:yyyyMMdd_HHmmss}.xml";
+                string filePath = Path.Combine(folderPath, fileName);
+
+                xml.Save(filePath);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Error al exportar XML de obras: " + ex.Message);
             }
         }
-
         private void BindObras(IEnumerable<Obra> obras)
         {
             var list = obras?.ToList() ?? new List<Obra>();
